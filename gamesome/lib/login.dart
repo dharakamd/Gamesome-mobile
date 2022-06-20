16581dc495d7ac6e13c3ef72610813dda50d31c6
Dharaka Pranamya Mahadhika Damar <dharaka45@gmail.com>
2022-06-20 17:45:37 +0700

    create navigator button

---
+++ gamesome/lib/login.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello asdasdsadWorld'),
+        ),
+      ),
+    );
+  }
+}
+
+class login extends StatelessWidget {
+  const login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('LOGIN'),
+        ),
+      ),
+    );
+  }
+}
